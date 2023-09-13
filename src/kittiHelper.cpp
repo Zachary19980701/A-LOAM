e47c01a33fe21b6806902b710af6e910a50ee29a
Zachary19980701 <15536896309@163.com>
2023-09-13 09:16:46 +0800

    更新注释

--- src/kittiHelper.cpp
+++ src/kittiHelper.cpp
@@ -22,7 +22,7 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <sensor_msgs/PointCloud2.h>
 
-std::vector<float> read_lidar_data(const std::string lidar_data_path)
+std::vector<float> read_lidar_data(const std::string lidar_data_path) //读取雷达数据
 {
     std::ifstream lidar_data_file(lidar_data_path, std::ifstream::in | std::ifstream::binary);
     lidar_data_file.seekg(0, std::ios::end);
@@ -103,6 +103,8 @@ int main(int argc, char** argv)
         std::stringstream pose_stream(line);
         std::string s;
         Eigen::Matrix<double, 3, 4> gt_pose; //初始化gt_pose
+        
+        //对真值矩阵中的每一个元素使用读取的pose_stream进行赋值
         for (std::size_t i = 0; i < 3; ++i)
         {
             for (std::size_t j = 0; j < 4; ++j)
@@ -112,11 +114,14 @@ int main(int argc, char** argv)
             }
         }
 
+        //使用相机的变换矩阵对相机的图像参数进行标定
         Eigen::Quaterniond q_w_i(gt_pose.topLeftCorner<3, 3>());
-        Eigen::Quaterniond q = q_transform * q_w_i;
-        q.normalize();
-        Eigen::Vector3d t = q_transform * gt_pose.topRightCorner<3, 1>();
+        Eigen::Quaterniond q = q_transform * q_w_i;//获取图像的最终变换矩阵
+        q.normalize();//进行归一化
+        Eigen::Vector3d t = q_transform * gt_pose.topRightCorner<3, 1>(); //进行平移
 
+
+        //初始化odomGT的ROS参数
         odomGT.header.stamp = ros::Time().fromSec(timestamp);
         odomGT.pose.pose.orientation.x = q.x();
         odomGT.pose.pose.orientation.y = q.y();
@@ -125,29 +130,33 @@ int main(int argc, char** argv)
         odomGT.pose.pose.position.x = t(0);
         odomGT.pose.pose.position.y = t(1);
         odomGT.pose.pose.position.z = t(2);
-        pubOdomGT.publish(odomGT);
+        pubOdomGT.publish(odomGT); //发布地面真值话题
 
+
+        //初始化poseGT话题
         geometry_msgs::PoseStamped poseGT;
         poseGT.header = odomGT.header;
         poseGT.pose = odomGT.pose.pose;
         pathGT.header.stamp = odomGT.header.stamp;
-        pathGT.poses.push_back(poseGT);
-        pubPathGT.publish(pathGT);
+        pathGT.poses.push_back(poseGT); //发布poseGT
+        pubPathGT.publish(pathGT); //发布pathGT
 
-        // read lidar point cloud
-        std::stringstream lidar_data_path;
+        // read lidar point cloud， 读取点云
+        std::stringstream lidar_data_path; //初始化点云路径
         lidar_data_path << dataset_folder << "velodyne/sequences/" + sequence_number + "/velodyne/" 
-                        << std::setfill('0') << std::setw(6) << line_num << ".bin";
-        std::vector<float> lidar_data = read_lidar_data(lidar_data_path.str());
-        std::cout << "totally " << lidar_data.size() / 4.0 << " points in this lidar frame \n";
+                        << std::setfill('0') << std::setw(6) << line_num << ".bin"; //读取点云路径
+        std::vector<float> lidar_data = read_lidar_data(lidar_data_path.str()); //读取点云
+        std::cout << "totally " << lidar_data.size() / 4.0 << " points in this lidar frame \n"; //输出一下点云信息，对点云信息归一化一下
 
-        std::vector<Eigen::Vector3d> lidar_points;
-        std::vector<float> lidar_intensities;
-        pcl::PointCloud<pcl::PointXYZI> laser_cloud;
+
+        //这里为什么要设立lidar_points和lidar_intensities两个变量来表示点云之后再初始化一个pcl的点云类型呢？
+        std::vector<Eigen::Vector3d> lidar_points; //初始化一个三维空间向量，表示点云的点的位置
+        std::vector<float> lidar_intensities; //雷达的反射强度，根据雷达的反射强度，可以进行滤波等操作
+        pcl::PointCloud<pcl::PointXYZI> laser_cloud; //初始化点云类型
         for (std::size_t i = 0; i < lidar_data.size(); i += 4)
         {
-            lidar_points.emplace_back(lidar_data[i], lidar_data[i+1], lidar_data[i+2]);
-            lidar_intensities.push_back(lidar_data[i+3]);
+            lidar_points.emplace_back(lidar_data[i], lidar_data[i+1], lidar_data[i+2]); //点云泪痣
+            lidar_intensities.push_back(lidar_data[i+3]); //点云强度
 
             pcl::PointXYZI point;
             point.x = lidar_data[i];
@@ -157,12 +166,14 @@ int main(int argc, char** argv)
             laser_cloud.push_back(point);
         }
 
-        sensor_msgs::PointCloud2 laser_cloud_msg;
-        pcl::toROSMsg(laser_cloud, laser_cloud_msg);
-        laser_cloud_msg.header.stamp = ros::Time().fromSec(timestamp);
-        laser_cloud_msg.header.frame_id = "/camera_init";
-        pub_laser_cloud.publish(laser_cloud_msg);
+        sensor_msgs::PointCloud2 laser_cloud_msg; //进行ROS发布的初始化
+        pcl::toROSMsg(laser_cloud, laser_cloud_msg); //从pcl转化到ROS类型
+        laser_cloud_msg.header.stamp = ros::Time().fromSec(timestamp); //获取时间戳
+        laser_cloud_msg.header.frame_id = "/camera_init"; 
+        pub_laser_cloud.publish(laser_cloud_msg); //发布点云话题
 
+
+        //发布左侧和右侧的图像话题
         sensor_msgs::ImagePtr image_left_msg = cv_bridge::CvImage(laser_cloud_msg.header, "mono8", left_image).toImageMsg();
         sensor_msgs::ImagePtr image_right_msg = cv_bridge::CvImage(laser_cloud_msg.header, "mono8", right_image).toImageMsg();
         pub_image_left.publish(image_left_msg);
@@ -181,7 +192,7 @@ int main(int argc, char** argv)
         r.sleep();
     }
     bag_out.close();
-    std::cout << "Done \n";
+    std::cout << "Done \n"; //发布结束
 
 
     return 0;
